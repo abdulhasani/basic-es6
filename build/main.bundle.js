ccd570b6e78036b942c310ca7030aad09e7751c4
Hasani <abdul.kadir.hasani@basajans.com>
2017-03-08 02:23:41 +0700

    Destructur membantu menghilangkan kebutuhan variabel sementara ketika bekerja dengan objek dan array

--- build/main.bundle.js
+++ build/main.bundle.js
@@ -3,19 +3,24 @@
 /**
  * Created by Hasani on 3/8/2017.
  */
-var calculateMonthlyPayment = function calculateMonthlyPayment(principal, year, rate) {
+var calculateMonthlyPayment = function calculateMonthlyPayment(principal, years, rate) {
   var monthlyRate = 0;
   if (rate) {
     monthlyRate = rate / 100 / 12;
   }
-  var monthlyPayment = principal * monthlyRate / (1 - Math.pow(1 / (1 + monthlyRate), year * 12));
-  return monthlyPayment;
+  var monthlyPayment = principal * monthlyRate / (1 - Math.pow(1 / (1 + monthlyRate), years * 12));
+  return { principal: principal, years: years, rate: rate, monthlyPayment: monthlyPayment, monthlyRate: monthlyRate };
 };
 
 document.getElementById('calcBtn').addEventListener('click', function () {
   var principal = document.getElementById("principal").value;
   var years = document.getElementById("years").value;
   var rate = document.getElementById("rate").value;
-  var monthlyPayment = calculateMonthlyPayment(principal, years, rate);
+
+  var _calculateMonthlyPaym = calculateMonthlyPayment(principal, years, rate),
+      monthlyPayment = _calculateMonthlyPaym.monthlyPayment,
+      monthlyRate = _calculateMonthlyPaym.monthlyRate;
+
   document.getElementById("monthlyPayment").innerHTML = monthlyPayment.toFixed(2);
+  document.getElementById("monthlyRate").innerHTML = (monthlyRate * 100).toFixed(2);
 });
--- js/main.js
+++ js/main.js
@@ -1,19 +1,30 @@
 /**
  * Created by Hasani on 3/8/2017.
  */
-var calculateMonthlyPayment = function(principal,year,rate){
+var calculateMonthlyPayment = function(principal,years,rate){
     let monthlyRate = 0;
     if(rate){
             monthlyRate = rate / 100 / 12;
     }
-    var monthlyPayment = principal * monthlyRate / (1-Math.pow(1/(1+monthlyRate),year * 12));
-    return monthlyPayment;
+    var monthlyPayment = principal * monthlyRate / (1-Math.pow(1/(1+monthlyRate),years * 12));
+    /**
+     * Destructur membantu menghilangkan kebutuhan
+     * variabel sementara ketika bekerja dengan objek dan array
+     */
+    return {principal,years,rate,monthlyPayment,monthlyRate};
 };
-
+/**
+ * ajax
+ */
 document.getElementById('calcBtn').addEventListener('click',function(){
    var principal = document.getElementById("principal").value;
     var years = document.getElementById("years").value;
     var rate = document.getElementById("rate").value;
-    var monthlyPayment = calculateMonthlyPayment(principal,years,rate);
+    /**
+     * menangkap json
+     * @type {{principal, years, rate, monthlyPayment, monthlyRate}}
+     */
+    let {monthlyPayment,monthlyRate} = calculateMonthlyPayment(principal,years,rate);
     document.getElementById("monthlyPayment").innerHTML = monthlyPayment.toFixed(2);
+    document.getElementById("monthlyRate").innerHTML = (monthlyRate * 100).toFixed(2);
 });
