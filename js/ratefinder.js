059e646d4d905d0f4ccf9114ce86bc9b68252563
Hasani <abdul.kadir.hasani@basajans.com>
2017-03-08 16:53:27 +0700

    example 1 promise

---
+++ js/ratefinder.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Hasani on 3/8/2017.
+ */
+let url = 'rates.json'
+fetch(url)
+    .then(response => response.json())
+    .then(rates => {
+        let html = '';
+        rates.forEach(rate => html += `
+            <tr>
+            <td>${rate.name}</td>
+            <td>${rate.years}</td>
+            <td>${rate.rate}%</td>
+            </tr>`)
+        document.getElementById('rates').innerHTML = html;
+    })
+    .catch(e => console.log(e));
